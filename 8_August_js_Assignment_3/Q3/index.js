054c9495a68c89ce0523e024d3f3a2efafbb7881
eshan-009 <eshansharma009@gmail.com>
2024-08-08 14:54:53 +0530

    submission

--- 8_August_js_Assignment_3/Q3/index.js
+++ 8_August_js_Assignment_3/Q3/index.js
@@ -1,6 +1,7 @@
 const showposition = (guess,random,guesscount)=>{
     if(guess === random)
         {
+            arr.push(guesscount);
             alert(`Congratulations you guessed it right \n Your Number of guesses are ${guesscount}`) ;
         }
         else if(Math.abs(guess-random)<10)
@@ -22,7 +23,7 @@ const showposition = (guess,random,guesscount)=>{
 const generateRandom = ()=>(Math.floor(Math.random()*100));
 const guessIt = ()=>parseInt(prompt("Guess the number between 0 to 100"));
 const playagain = ()=>parseInt(prompt("Enter 1 to play again"));
-const startgame = (start,guess,guesscount)=>{
+const startgame = (start,guess,guesscount,arr)=>{
     do
 {    
 const random = generateRandom();
@@ -32,7 +33,9 @@ while(guess!==random)
         guess = guessIt();
         guesscount++;
         showposition(guess,random,guesscount); 
+        
     }
+   
     start=0;
     guess=-1;
     guesscount=0;
@@ -47,7 +50,9 @@ alert("Welcome to the Game");
 var start=1;
 var guess=-1;
 var guesscount =0;
-
+var arr = [];
 
 
 startgame(start,guess,guesscount); 
+
+console.log(arr);
